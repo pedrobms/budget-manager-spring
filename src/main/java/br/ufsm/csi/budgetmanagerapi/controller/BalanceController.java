d5bb868e66d3c10308e9c88c2770ea5d8a8d8e2e
Pedro Bilar Montero <pedro.bilar.montero@gmail.com>
2023-01-05 11:13:07 -0300

    Add balance endpoints

--- src/main/java/br/ufsm/csi/budgetmanagerapi/controller/BalanceController.java
+++ src/main/java/br/ufsm/csi/budgetmanagerapi/controller/BalanceController.java
@@ -3,15 +3,18 @@ package br.ufsm.csi.budgetmanagerapi.controller;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ufsm.csi.budgetmanagerapi.model.TransactionType;
 import br.ufsm.csi.budgetmanagerapi.service.BalanceService;
 
 @RestController
 @RequestMapping("/users/{userId}/balance")
+@CrossOrigin(origins = "http://localhost:8081")
 public class BalanceController {
     @Autowired
     private BalanceService balanceService;
@@ -19,5 +22,20 @@ public class BalanceController {
     @GetMapping("")
     public BigDecimal getBalance(@PathVariable Long userId) {
         return balanceService.getBalance(userId);
+    }
+
+    @GetMapping("/income")
+    public BigDecimal getIncome(@PathVariable Long userId) {
+        return balanceService.getSumOfTransactionsByType(userId, TransactionType.INCOME);
+    }
+
+    @GetMapping("/expense")
+    public BigDecimal getExpense(@PathVariable Long userId) {
+        return balanceService.getSumOfTransactionsByType(userId, TransactionType.EXPENSE);
+    }
+
+    @GetMapping("/month/{month}")
+    public BigDecimal getBalanceByMonth(@PathVariable Long userId, @PathVariable int month) {
+        return balanceService.getSumOfTransactionsByMonth(userId, month);
     }
 }
